7f4daed4ce141eac4e77832e4ba4a9443c3dead5
Student User <student@workstation.lab.example.com>
2022-08-30 08:42:03 -0400

    comment update

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,10 +4,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
+  res.send('Hello Wor__ld from pod: ' + process.env.HOSTNAME + '\n')
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listen__ing on port 8080!');
 });
 
